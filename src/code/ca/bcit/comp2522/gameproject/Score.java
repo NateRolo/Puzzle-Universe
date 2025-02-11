b5b4f90581888abd7ad198a21458993d03d10820
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-02-11 13:54:58 -0800

    feat: write askQuestion(), modularize question styles.

--- src/code/ca/bcit/comp2522/gameproject/Score.java
+++ src/code/ca/bcit/comp2522/gameproject/Score.java
@@ -19,7 +19,7 @@ public class Score
     private int numGamesPlayed;
     private int numCorrectFirstAttempt;
     private int numCorrectSecondAttempt;
-    private int numIncorrectTwoAttempts;
+    private int numIncorrectAttempts;
     
     final LocalDateTime currentTime;
     final DateTimeFormatter formatter;
@@ -37,7 +37,7 @@ public class Score
         this.numGamesPlayed = 0;
         this.numCorrectFirstAttempt = 0;
         this.numCorrectSecondAttempt = 0;
-        this.numIncorrectTwoAttempts = 0;
+        this.numIncorrectAttempts = 0;
     }
 
     /**
@@ -85,9 +85,9 @@ public class Score
      *
      * @return number of incorrect attempts after two tries
      */
-    public final int getNumIncorrectTwoAttempts()
+    public final int getNumIncorrectAttempts()
     {
-        return numIncorrectTwoAttempts;
+        return numIncorrectAttempts;
     }
 
     /**
@@ -117,8 +117,8 @@ public class Score
     /**
      * Increments the number of incorrect attempts after two tries.
      */
-    public final void incrementNumIncorrectTwoAttempts()
+    public final void incrementNumIncorrectAttempts()
     {
-        this.numIncorrectTwoAttempts++;
+        this.numIncorrectAttempts++;
     }
 }
--- src/code/ca/bcit/comp2522/gameproject/WordGame.java
+++ src/code/ca/bcit/comp2522/gameproject/WordGame.java
@@ -26,37 +26,104 @@ public class WordGame
 
     static void play()
     {
+        int questionsAsked;
+
+        questionsAsked = 0;
+        
         System.out.println("Starting Word Game...");
 
     }
 
-    static void capitalCityQuestion()
+    private static void askQuestion()
     {
         final Country thisCountry;
-        final String countryName;
-        final String countryCapital;
-        final String userAnswer;
-        final boolean answerIsCorrect;
+        final int questionStyle;
 
         thisCountry = world.getRandomCountry();
-        countryName = thisCountry.getName();
-        countryCapital = thisCountry.getCapitalCityName();
+        questionStyle = (int) (Math.random() * 3);
+        
+        switch(questionStyle)
+        {
+            case 0 -> capitalCityQuestion(thisCountry); 
+            case 1 -> countryQuestion(thisCountry);
+            case 2 -> factQuestion(thisCountry);
+        }
+    }
+
+    private static void capitalCityQuestion(final Country country)
+    {
+        final String countryName;
+        final String countryCapital;
+        
+        countryName = country.getName();
+        countryCapital = country.getCapitalCityName();
 
         System.out.println(countryCapital + " is the capial of what country?");
-        userAnswer = scan.nextLine();
-
-        answerIsCorrect = userAnswer.equals(countryName);
-
-        if(answerIsCorrect)
-        {
-            System.out.println("That's correct!" + 
-                            " +2 points.");
-        }
-        else
-        {
-            System.out.println("Incorrect! One more try...");
-        }
-        
-        
+        checkAnswer(countryName);
     }
+
+    private static void countryQuestion(final Country country)
+    {
+        final String countryName;
+        final String countryCapital;
+
+        countryName = country.getName();
+        countryCapital = country.getCapitalCityName();
+
+        System.out.println("What is the capital of " + countryName + "?");
+        checkAnswer(countryCapital);
+    }
+
+    private static void factQuestion(final Country country)
+    {
+        final String countryName;
+        final String[] countryFacts;
+        final int randomFact;
+
+        countryName = country.getName();
+        countryFacts = country.getFacts();
+        randomFact = (int)(Math.random() * countryFacts.length);
+
+        System.out.println(countryFacts[randomFact] + 
+                           "\n" + 
+                           "What country is this fact describing?");
+        checkAnswer(countryName);
+    }
+
+    /* 
+     * Checks if the user's answer matches the expected answer.
+     * Returns the number of points earned.
+     */
+    private static void checkAnswer(final String expectedAnswer)
+    {
+        String userAnswer;
+        boolean answerIsCorrect;
+        
+        userAnswer = scan.nextLine();
+        answerIsCorrect = userAnswer.equals(expectedAnswer);
+
+        if (answerIsCorrect)
+        {
+            System.out.println("CORRECT");
+            currentScore.incrementNumCorrectFirstAttempt();
+        }
+
+        
+        System.out.println("INCORRECT" + 
+                           "\nOne more guess:");
+        userAnswer = scan.nextLine();
+        answerIsCorrect = userAnswer.equals(expectedAnswer);
+        currentScore.incrementNumIncorrectAttempts();
+
+        if (answerIsCorrect)
+        {
+            System.out.println("CORRECT");
+            currentScore.incrementNumCorrectSecondAttempt();
+        }
+        
+        System.out.println("INCORRECT");
+        currentScore.incrementNumIncorrectAttempts();
+    }
+
+    
 }
