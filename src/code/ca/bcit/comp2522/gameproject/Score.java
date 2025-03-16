7c4e7087d9f3938f66a0c741927bff9cdd9e8d96
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-16 00:25:58 -0700

    feat: implement constructors to handle all params, no score, no params

--- src/code/ca/bcit/comp2522/gameproject/Score.java
+++ src/code/ca/bcit/comp2522/gameproject/Score.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 /**
  * Represents a player's score in the game.
@@ -18,31 +20,81 @@ import java.util.List;
  */
 public class Score
 {
+    private final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+    private static final int DEFAULT_GAMES_PLAYED             = 0;
+    private static final int DEFAULT_CORRECT_FIRST_GUESSES = 0;
+    private static final int DEFAULT_CORRECT_SECOND_GUESSES = 0;
+    private static final int DEFAULT_INCORRECT_TWO_TIMES    = 0;
+    private static final int DEFAULT_SCORE              = 0;
+
     private static final int CORRECT_FIRST_GUESS_SCORE  = 2;
     private static final int CORRECT_SECOND_GUESS_SCORE = 1;
+
     private              int numGamesPlayed;
     private              int numCorrectFirstAttempt;
     private              int numCorrectSecondAttempt;
     private              int numIncorrectTwoAttempts;
     private              int score;
 
-    final LocalDateTime     currentTime;
-    final DateTimeFormatter formatter;
     final String            formattedDateTime;
+
+    public Score(final LocalDateTime dateTime,
+                 final int gamesPlayed,
+                 final int numCorrectFirstGuess,
+                 final int numCorrectSecondGuess,
+                 final int twoIncorrectAttempts,
+                 final int totalScore)
+    {
+        validateDateTime(dateTime);
+        validateNumber(gamesPlayed);
+        validateNumber(numCorrectFirstGuess);
+        validateNumber(numCorrectSecondGuess);
+        validateNumber(twoIncorrectAttempts);
+
+        formattedDateTime = dateTime.format(formatter);
+        this.numGamesPlayed = gamesPlayed;
+        numCorrectFirstAttempt = numCorrectFirstGuess;
+        numCorrectSecondAttempt = numCorrectSecondGuess;
+        numIncorrectTwoAttempts = twoIncorrectAttempts;
+        this.score = totalScore;
+    }
+
+    public Score(final LocalDateTime dateTime,
+                 final int gamesPlayed,
+                 final int numCorrectFirstGuess,
+                 final int numCorrectSecondGuess,
+                 final int twoIncorrectAttempts)
+    {
+        this(dateTime,
+             gamesPlayed,
+             numCorrectFirstGuess,
+             numCorrectSecondGuess,
+             twoIncorrectAttempts,
+             DEFAULT_SCORE);
+    }
 
     /**
      * Constructs a new Score object.
      */
     public Score()
     {
-        this.currentTime  = LocalDateTime.now();
-        formatter         = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        formattedDateTime = currentTime.format(formatter);
+        this(LocalDateTime.now(),
+              DEFAULT_GAMES_PLAYED,
+              DEFAULT_CORRECT_FIRST_GUESSES,
+              DEFAULT_CORRECT_SECOND_GUESSES,
+              DEFAULT_INCORRECT_TWO_TIMES,
+              DEFAULT_SCORE);
+    }
 
-        this.numGamesPlayed          = 0;
-        this.numCorrectFirstAttempt  = 0;
-        this.numCorrectSecondAttempt = 0;
-        this.numIncorrectTwoAttempts = 0;
+    private void validateNumber(final int gamesPlayed)
+    {
+
+    }
+
+    private void validateDateTime(final LocalDateTime dateTime)
+    {
+
     }
 
     /**
@@ -50,9 +102,9 @@ public class Score
      *
      * @return the date and time played
      */
-    public final LocalDateTime getCurrentTime()
+    public final String getCurrentTime()
     {
-        return currentTime;
+        return formattedDateTime;
     }
 
     /**
@@ -95,6 +147,10 @@ public class Score
         return numIncorrectTwoAttempts;
     }
 
+    public final int getScore()
+    {
+        return score;
+    }
     /**
      * Increments the number of games played.
      */
@@ -132,31 +188,50 @@ public class Score
     }
 
 
-    public final List<String> formatScore()
+    private static List<String> formatScore(Score score)
     {
-        final List<String> score;
-        score = new ArrayList<>();
+        final List<String> scoreAsList;
+        scoreAsList = new ArrayList<>();
 
-        score.add("Date and Time: " + this.formattedDateTime);
-        score.add("Games Played: " + this.numGamesPlayed);
-        score.add("Correct First Attempts: " + this.numCorrectFirstAttempt);
-        score.add("Correct Second Attempts: " + this.numCorrectSecondAttempt);
-        score.add("Incorrect Attempts: " + this.numIncorrectTwoAttempts);
-        score.add("Score: " + this.score);
+        scoreAsList.add("Date and Time: " + score.formattedDateTime);
+        scoreAsList.add("Games Played: " + score.numGamesPlayed);
+        scoreAsList.add("Correct First Attempts: " + score.numCorrectFirstAttempt);
+        scoreAsList.add("Correct Second Attempts: " + score.numCorrectSecondAttempt);
+        scoreAsList.add("Incorrect Attempts: " + score.numIncorrectTwoAttempts);
+        scoreAsList.add("Score: " + score.score + " points");
 
-        return score;
+        return scoreAsList;
     }
 
-    public final void appendScoreToFile() throws IOException
+    public static void appendScoreToFile(Score score, String file) throws IOException
     {
-        final List<String> score;
-        score = this.formatScore();
+        final List<String> scoreAsList;
+        scoreAsList = formatScore(score);
 
-        FileManager.writeToResource(score);
+        FileManager.writeToResource(scoreAsList, file);
     }
 
     public final void printScore()
     {
-        this.formatScore().forEach(System.out::println);
+        formatScore(this).forEach(System.out::println);
+    }
+
+    public static List<Score> readScoresFromFile(final String filePath) throws IOException
+    {
+        final List<String> scoresLines;
+        final List<Score> scores;
+        scoresLines = FileManager.readLinesFromResource("/" + filePath);
+
+        scores = IntStream.range(0, scoresLines.size())
+                          .filter(i -> scoresLines.get(i).startsWith("Date and Time:"))
+                          .mapToObj(i -> new Score(
+                                  LocalDateTime.parse(scoresLines.get(i).split(": ", 2)[1], formatter),
+                                  Integer.parseInt(scoresLines.get(i + 1).split(": ", 2)[1]),
+                                  Integer.parseInt(scoresLines.get(i + 2).split(": ", 2)[1]),
+                                  Integer.parseInt(scoresLines.get(i + 3).split(": ", 2)[1]),
+                                  Integer.parseInt(scoresLines.get(i + 4).split(": ", 2)[1]),
+                                  Integer.parseInt(scoresLines.get(i + 5).split(": ", 2)[1].replace(" points", ""))
+                          )).toList();
+        return scores;
     }
 }
