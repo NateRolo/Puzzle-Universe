522df0d302287bd8a8c736ada975117c7b5e6a78
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-23 18:44:23 -0700

    docs: write javadocs

--- src/code/ca/bcit/comp2522/gameproject/wordGame/CapitalCityQuestion.java
+++ src/code/ca/bcit/comp2522/gameproject/wordGame/CapitalCityQuestion.java
@@ -1,18 +1,43 @@
 package ca.bcit.comp2522.gameproject.wordGame;
 
+/**
+ * Represents a question about identifying a country based on its capital city.
+ * <p>
+ * This class extends the Question class to create questions where the user
+ * is given a capital city name and must identify the corresponding country.
+ * </p>
+ *
+ * @author Nathan O
+ * @version 1.0 2025
+ */
 class CapitalCityQuestion extends Question
 {
+    /**
+     * Constructs a new CapitalCityQuestion with the specified country.
+     *
+     * @param country the Country object containing the data for this question
+     */
     CapitalCityQuestion(Country country)
     {
         super(country);
     }
 
+    /**
+     * Creates the question prompt for this capital city question.
+     *
+     * @return a string containing the question prompt
+     */
     @Override
-    String getPrompt()
+    String createPrompt()
     {
         return country.getCapitalCityName() + "is the capital of what country?";
     }
 
+    /**
+     * Gets the expected answer for this question.
+     *
+     * @return the name of the country as the expected answer
+     */
     @Override
     String getExpectedAnswer()
     {
