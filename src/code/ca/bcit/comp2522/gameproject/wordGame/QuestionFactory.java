c5d56b71e294617396e93b4756cf9f449d185896
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-23 18:45:43 -0700

    docs: write javadocs for code readability

--- src/code/ca/bcit/comp2522/gameproject/wordGame/QuestionFactory.java
+++ src/code/ca/bcit/comp2522/gameproject/wordGame/QuestionFactory.java
@@ -1,9 +1,31 @@
 package ca.bcit.comp2522.gameproject.wordGame;
 
-class QuestionFactory
+/**
+ * Factory class for creating different types of questions.
+ * <p>
+ * This class handles the creation of specific question types based on the
+ * provided question style and country data. It ensures proper instantiation
+ * of the correct question subclass.
+ * </p>
+ *
+ * @author Nathan O
+ * @version 1.0 2025
+ */
+final class QuestionFactory
 {
+    /**
+     * Creates a new question of the specified type for the given country.
+     * <p>
+     * This method validates the country parameter and creates the appropriate
+     * question type based on the questionType parameter.
+     * </p>
+     *
+     * @param country      the country to create a question about
+     * @param questionType the type of question to create (must be one of the constants defined in WordGameLauncher)
+     * @return a new Question instance of the specified type
+     */
     static Question createQuestion(final Country country,
-                                          final int questionType)
+                                   final int questionType)
     {
         validateCountry(country);
 
@@ -16,6 +38,15 @@ class QuestionFactory
         };
     }
 
+    /**
+     * Validates that the country parameter is not null.
+     * <p>
+     * This method ensures that a valid Country object is provided before
+     * attempting to create a question.
+     * </p>
+     *
+     * @param country the Country object to validate
+     */
     private static void validateCountry(final Country country)
     {
         if(country == null)
