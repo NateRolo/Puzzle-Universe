ec61e7845147b4dae15968ab8f73366a45b118ce
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-23 18:43:40 -0700

    feat: implement SecretCode

--- src/code/ca/bcit/comp2522/gameproject/mastermind/SecretCode.java
+++ src/code/ca/bcit/comp2522/gameproject/mastermind/SecretCode.java
@@ -1,4 +1,57 @@
 package ca.bcit.comp2522.gameproject.mastermind;
 
-public class SecretCode {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * Represents the secret code that players must guess in Mastermind.
+ * <p>
+ * This class extends the base Code class and provides functionality to generate
+ * random secret codes of specified lengths.
+ * </p>
+ *
+ * @author Nathan O
+ * @version 1.0 2025
+ */
+final class SecretCode extends Code
+{
+    private static final int MIN_DIGIT = 1;
+    private static final int MAX_DIGIT = 6;
+    private static final int MIN_CODE_LENGTH = 4;
+    private static final Random RANDOM = new Random();
+
+    /**
+     * Constructs a new SecretCode with the specified digits.
+     *
+     * @param digits the sequence of digits that make up the code
+     */
+    private SecretCode(final List<Integer> digits)
+    {
+        super(digits);
+    }
+
+    /**
+     * Generates a random secret code of the specified length.
+     *
+     * @param length the desired length of the secret code
+     * @return a new SecretCode with random digits
+     */
+    static SecretCode generateRandomCode(final int length)
+    {
+        if (length < MIN_CODE_LENGTH)
+        {
+            throw new IllegalArgumentException("Code length must be at least 4");
+        }
+
+        final List<Integer> randomDigits;
+        
+        randomDigits = new ArrayList<>();
+        for (int i = 0; i < length; i++)
+        {
+            randomDigits.add(RANDOM.nextInt(MAX_DIGIT) + MIN_DIGIT);
+        }
+
+        return new SecretCode(randomDigits);
+    }
 }
