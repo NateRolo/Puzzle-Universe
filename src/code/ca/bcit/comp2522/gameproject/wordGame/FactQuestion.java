542252c6eaa80dc6a7d7c713dfbe556909e59d15
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-23 18:45:16 -0700

    docs: write javadocs for code readability

--- src/code/ca/bcit/comp2522/gameproject/wordGame/FactQuestion.java
+++ src/code/ca/bcit/comp2522/gameproject/wordGame/FactQuestion.java
@@ -1,23 +1,50 @@
 package ca.bcit.comp2522.gameproject.wordGame;
 
+/**
+ * Represents a question about identifying a country based on an interesting fact.
+ * <p>
+ * This class extends the Question class to create questions where the user
+ * is given a random fact about a country and must identify which country
+ * the fact is describing.
+ * </p>
+ *
+ * @author Nathan O
+ * @version 1.0 2025
+ */
 class FactQuestion extends Question
 {
     private final int factIndex;
 
+    /**
+     * Constructs a new FactQuestion with the specified country.
+     * Randomly selects one of the country's facts to use in the question.
+     *
+     * @param country the Country object containing the data for this question
+     */
     FactQuestion(Country country)
     {
         super(country);
         this.factIndex = (int)(Math.random() * country.getFacts().length);
     }
 
+    /**
+     * Creates the question prompt for this fact question.
+     *
+     * @return a string containing the question prompt with the selected fact
+     */
     @Override
-    String getPrompt()
+    protected String createPrompt()
     {
         return country.getFacts()[factIndex] + "\nWhat country is this fact describing?";
     }
 
+    /**
+     * Gets the expected answer for this question.
+     *
+     * @return the name of the country as the expected answer
+     */
     @Override
-    String getExpectedAnswer()
+    protected String getExpectedAnswer()
     {
         return country.getName();
     }
