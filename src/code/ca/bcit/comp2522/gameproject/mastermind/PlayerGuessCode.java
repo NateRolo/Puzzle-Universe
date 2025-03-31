adb3a7f39bf46cf34093addd94fde49901fb4377
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-31 00:25:37 -0700

    refactor: remove truth scan logic from PlayerGuessCode
    - Removed truth scan constructors, flag, and logic from PlayerGuessCode
    - PlayerGuessCode now exclusively represents a digit-based player guess

--- src/code/ca/bcit/comp2522/gameproject/mastermind/PlayerGuessCode.java
+++ src/code/ca/bcit/comp2522/gameproject/mastermind/PlayerGuessCode.java
@@ -13,12 +13,8 @@ import java.util.List;
  * @version 1.0 2025
  */
 public final class PlayerGuessCode extends
-                               Code
+                               Code implements PlayerAction
 {
-    private static final String TRUTH_SCAN_ERROR  = "Cannot get digits from truth scan request";
-
-    private final boolean isTruthScanRequest;
-
     /**
      * Constructs a PlayerGuessCode representing a guess.
      *
@@ -27,51 +23,5 @@ public final class PlayerGuessCode extends
     public PlayerGuessCode(final List<Integer> digits)
     {
         super(digits);
-        this.isTruthScanRequest = false;
-    }
-
-    /**
-     * Creates a PlayerGuessCode representing a truth scan request.
-     *
-     * @return a new PlayerGuessCode configured as a truth scan request
-     */
-    public static PlayerGuessCode createTruthScanRequest()
-    {
-        return new PlayerGuessCode();
-    }
-
-    /*
-     * Private constructor for truth scan request.
-     */
-    private PlayerGuessCode()
-    {
-        super(null);
-        this.isTruthScanRequest = true;
-    }
-
-    /**
-     * Gets the digits of the guess.
-     *
-     * @return the list of digits
-     * @throws IllegalStateException if this input is a truth scan request
-     */
-    @Override
-    public List<Integer> getDigits()
-    {
-        if(isTruthScanRequest)
-        {
-            throw new IllegalStateException(TRUTH_SCAN_ERROR);
-        }
-        return super.getDigits();
-    }
-
-    /**
-     * Checks if this input is a truth scan request.
-     *
-     * @return true if this is a truth scan request, false if it's a guess
-     */
-    public boolean isTruthScanRequest()
-    {
-        return isTruthScanRequest;
     }
 }
