abd1ecd1988884bf0a4a8bb2e24b48cc87a03cc1
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-03-31 21:24:44 -0700

    refactor: restructure PlayerAction interface and remove GuessSummaryRequest and TruthScanRequest classes
    - Deleted GuessSummaryRequest class and integrated it as a static inner class within PlayerAction interface for better organization
    - Removed TruthScanRequest class to streamline the codebase
    - Enhanced the structure of PlayerAction by consolidating related actions

--- src/code/ca/bcit/comp2522/gameproject/mastermind/PlayerAction.java
+++ src/code/ca/bcit/comp2522/gameproject/mastermind/PlayerAction.java
@@ -2,4 +2,11 @@ package ca.bcit.comp2522.gameproject.mastermind;
 
 public interface PlayerAction 
 {
+    public static final class TruthScanRequest implements PlayerAction
+    {
+        public TruthScanRequest(){}
+    }
+
+    public static final class GuessSummaryRequest implements PlayerAction
+    {}
 }
