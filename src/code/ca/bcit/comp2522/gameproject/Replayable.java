e065e1d23bd44fffc1cb396978d63853c72bd16d
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2025-04-04 08:46:51 -0700

    feat: add Replayable interface for single game session functionality
    - Introduced the Replayable interface, extending Playable, to represent games that can be played repeatedly.
    - Added the playOneGame method to facilitate the execution of a single game session, enhancing the game's replayability.

--- src/code/ca/bcit/comp2522/gameproject/Replayable.java
+++ src/code/ca/bcit/comp2522/gameproject/Replayable.java
@@ -1,6 +1,16 @@
 package ca.bcit.comp2522.gameproject;
 
+/**
+ * Represents games that can be played repeatedly, one game at a time.
+ * Extends the basic {@link Playable} interface with the capability
+ * to initiate a single game session.
+ *
+ * @author Nathan O
+ */
 public interface Replayable extends Playable
 {
+    /**
+     * Executes a single session or round of the game.
+     */
     void playOneGame();
 }
